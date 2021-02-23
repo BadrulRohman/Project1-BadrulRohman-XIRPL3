9a8d5c7596e393366df9db8b6f8134ba405c317a
BadrulRohman <badrulmdr27@gmail.com>
2021-02-23 12:18:34 +0700

    implementasi kelas menu

--- Aplikasi Kasir/src/Classes/Menu.java
+++ Aplikasi Kasir/src/Classes/Menu.java
@@ -1,30 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Classes;
 
-/**
- *
- * @author Asus-X441S
- */
+
 public class Menu {
         private String nama_menu;
         private double harga;
-        private String katageori;
+        private String kategori;
         
-        public String getNama_menu() {return ""; }
+        public String getNama_menu() {
+            return nama_menu; 
+        }
         
-        public void setNama_menu(String nama_menu) {}
+        public void setNama_menu(String nama_menu) {
+        this.nama_menu =  nama_menu;
+        }
         
-        public double getHarga() {return 0; }
+        public double getHarga() {
+            return harga; 
+        }
         
-        void setHarga (double harga) {}
+        void setHarga (double harga) {
+        this.harga = harga;
+        }
         
-        public String getKategori () {return ""; }
+        public String getKategori () {
+            return kategori; 
+        }
         
-        public void setKategori (String kategori) {}
+        public void setKategori (String kategori) {
+        this.kategori = kategori;
+        }
               }
     
 
